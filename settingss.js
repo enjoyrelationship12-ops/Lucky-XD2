cc00799a2cdcadce57e189c35400c7840cbe8896
enjoyrelationship12-ops <enjoyrelationship12@gmail.com>
2025-09-21 00:11:02 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~d78AXYCZ#zhxj-h3bEQoIk1y5N6lokCBKDEPypPuEhVNfqUzpz_E",  // add your Session Id here
+  OWNER_NUMBER: "2348138895701", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
